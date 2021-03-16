ecb2b709c1b1b699255f830e81e1853e30914959
Error404NotFound <79180058+vckyou@users.noreply.github.com>
2021-03-16 15:55:52 +0700

    Update whois.py

--- userbot/modules/whois.py
+++ userbot/modules/whois.py
@@ -21,7 +21,7 @@ from userbot.events import register
 async def who(event):
 
     await event.edit(
-        "`Mengambil Informasi Pengguna Ini...`")
+        "`Mengambil Informasi Pengguna...`")
 
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
         os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
