08b06de254a209067007be1165a1acbf721bc82c
Error404NotFound <79180058+vckyou@users.noreply.github.com>
2021-03-16 15:31:36 +0700

    Update santet.py

--- userbot/modules/santet.py
+++ userbot/modules/santet.py
@@ -313,7 +313,7 @@ async def typewriter(typew):
     sleep(0.03)
     await typew.edit(str(number) + "%   ████████████████▌")
     sleep(1)
-    await typew.edit("`Target Berhasil Tersantet Online:v`")
+    await typew.edit("`Target Berhasil Tersantet Online`")
 
 
 CMD_HELP.update({
