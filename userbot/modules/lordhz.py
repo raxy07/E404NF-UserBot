82deffaf9a3f088e458d0383400c8c03122564f3
Error404NotFound <79180058+vckyou@users.noreply.github.com>
2021-03-16 14:30:11 +0700

    Update lordhz.py

--- userbot/modules/lordhz.py
+++ userbot/modules/lordhz.py
@@ -10,19 +10,19 @@ from userbot import bot, TEMP_DOWNLOAD_DIRECTORY, CMD_HELP
 
 @register(outgoing=True, pattern=r"^.hz(:? |$)(.*)?")
 async def _(hazmat):
-    await hazmat.edit("`Lord Ingin Mengaktifkan Perintah Hazmat ヅ`")
+    await hazmat.edit("`Lord Ingin Mengaktifkan Perintah Hazmat`")
     level = hazmat.pattern_match.group(2)
     if hazmat.fwd_from:
         return
     if not hazmat.reply_to_msg_id:
-        await hazmat.edit("`Lord, Mohon Balas Ke Sticker/Gambar ヅ`")
+        await hazmat.edit("`Lord, Mohon Balas Ke Sticker/Gambar`")
         return
     reply_message = await hazmat.get_reply_message()
     if not reply_message.media:
-        await hazmat.edit("`Kata Bisa Menghancurkan Apapun Lord ヅ`")
+        await hazmat.edit("`Kata Bisa Menghancurkan Apapun`")
         return
     chat = "@hazmat_suit_bot"
-    await hazmat.edit("```Perintah Hazmat Diaktifkan, Sedang Memproses.... ヅ```")
+    await hazmat.edit("```Perintah Hazmat Diaktifkan, Sedang Memproses....```")
     message_id_to_reply = hazmat.message.reply_to_msg_id
     msg_reply = None
     async with hazmat.client.conversation(chat) as conv:
@@ -40,10 +40,10 @@ async def _(hazmat):
             """ - don't spam notif - """
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await hazmat.reply("`Lord, Mohon Buka Blokir` @hazmat_suit_bot `Lalu Coba Lagi`")
+            await hazmat.reply("`Mohon Buka Blokir` @hazmat_suit_bot `Lalu Coba Lagi`")
             return
         if response.text.startswith("I can't"):
-            await hazmat.edit("`Mohon Maaf Lord, GIF Tidak Bisa...`")
+            await hazmat.edit("`Mohon Maaf, GIF Tidak Bisa...`")
             await hazmat.client.delete_messages(
                 conv.chat_id, [msg.id, response.id, r.id, msg_reply.id]
             )
